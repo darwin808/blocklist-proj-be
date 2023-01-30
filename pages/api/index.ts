f46595939662ad5b28c6b33a07d16c2a55675dfb
darwin808 <darwin.apolinario12@gmail.com>
2023-01-31 05:56:36 +0800

    initial

---
+++ pages/api/index.ts
@@ -0,0 +1,29 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import NextCors from "nextjs-cors";
+
+type Data = {
+  data: any;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  await NextCors(req, res, {
+    // Options
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
+  const { method } = req;
+  console.log(req.body, 44455555);
+  switch (method) {
+    case "GET":
+      res.status(200).json({ data: "GET" });
+      break;
+    case "POST":
+      res.status(200).json({ data: req.body });
+      break;
+  }
+}
