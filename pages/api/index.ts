362956121a89ad5857a540005a6016ccbab7d80d
darwin808 <darwin.apolinario12@gmail.com>
2023-01-31 05:58:41 +0800

    initia

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -17,7 +17,7 @@ export default async function handler(
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   });
   const { method } = req;
-  console.log(req.body, 44455555);
+  console.log(req.body, 4445555);
   switch (method) {
     case "GET":
       res.status(200).json({ data: "GET" });
